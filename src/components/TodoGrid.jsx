df51de1c83654f451728565ca49363677f1f9cc8
renanlcavalcante <renanluquete@gmail.com>
2016-10-22 17:48:29 -0200

    consumindo api (Concluido)

--- src/components/TodoGrid.jsx
+++ src/components/TodoGrid.jsx
@@ -3,11 +3,33 @@ import Localizar from './Localizar';
 import Card from './Card';
 
 class TodoGrid extends Component{
+	
+	state = {
+		todos:[]
+	}
+	
+	componentDidMount(){
+		fetch('https://private-00cf6-reacttodo.apiary-mock.com/lista')
+			.then(response => response.json())
+			.then(response => {
+				this.setState({
+					todos: response
+				})
+			})
+	}
+	
+	renderCards(){
+		return this.state.todos.map(todo => {
+			return <Card key={todo.id} 
+			titulo={todo.titulo} />
+		})
+	}
+	
 	render(){
 		return(
 			<div>
 				<Localizar texto="Localizar tarefa"/>
-				<Card titulo="Titulo legal"/>
+				{ this.renderCards() }
 			</div>
 		);
 	}
